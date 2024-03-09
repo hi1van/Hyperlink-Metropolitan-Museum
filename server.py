0826a9efc5189e24ace3bd0de9cea41b02e77441
Ivan Zhuang <ivan.c.zhuang@gmail.com>
2024-03-09 18:40:34 +1100

    first commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+import ArtPiece
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return 'Hello World!'
+
+
+if __name__ == "__main__":
+    serve(host="0.0.0.0", port=8000)
