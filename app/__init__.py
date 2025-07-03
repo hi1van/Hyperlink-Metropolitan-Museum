6d01da093d1e315ab7ed4e6caa2ddea415400f47
Ivan Zhuang <iczhuang@student.unimelb.edu.au>
2025-07-03 19:57:09 -0400

    added most comments throughout

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,27 @@ from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 from flask_migrate import Migrate
 
+# create global SQLAlchemy object
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+    """ 
+    factory function which creates and returns a new Flask instance
+    """
 
+    app = Flask(__name__)
+
+    # load application configurations
+    app.config.from_object(Config) 
+
+    # bind the SQLAlchemy db object to the Flask app
     db.init_app(app)
+    
     from .models import User
     from .models import Art
     migrate = Migrate(app, db)
     
+    # register the blueprint to make routes part of the app
     from .routes import main
     app.register_blueprint(main)
 
--- server.py
+++ server.py
@@ -3,9 +3,10 @@ from flask_migrate import Migrate
 from datetime import timedelta
 import os
 
+# create Flask application instance
 app = create_app()
 app.config['SECRET_KEY'] = os.urandom(24)
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30) # user session time limit
 
 migrate = Migrate(app, db)
 
